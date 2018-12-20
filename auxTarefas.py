8797413c57089d29fbda1df03e17cc37775cc49a
olecram86 <45532938+olecram86@users.noreply.github.com>
2018-12-20 15:05:03 +0000

    Add files via upload

--- auxTarefas.py
+++ auxTarefas.py
@@ -1,9 +1,29 @@
 import csv
 import datetime
+import os
+
+class myLists():
+    def __init__(self, nome, num, lista):
+        self.nome = nome
+        self.num = num
+        self.lista = lista
+
+def openTxt(nameFile):
+    #print("diretorio atual: ",os.path.abspath(file))
+    #print("diretorio sem o ultimo elemento: ",os.path.dirname(os.path.abspath(file)))
+    dir = os.path.dirname(os.path.abspath(__file__))   # tira o ultimo elemento que é o nome do programa atual
+    #print("dir 1: ",dir)
+    dir = os.path.dirname(dir) # tira o ultimo elemento que é o nome da pasta dos códigos
+    #print("dir 2: ",dir)
+
+    dir = dir +r"\dados"+nameFile # adicionamos a pasta e o nome do ficheiro que queremos abrir (tem de ter o r atrás porque o python interpreta \f como um caracter esepcial)
+    #print("novo diretorio: ",dir)
+    return dir
 
 def readFunc():
     list=[]
-    myFile = open("tarefas.txt", "r")
+    dir = openTxt(r"\tarefas.txt")
+    myFile = open(os.path.join(dir),"r")
     reader = csv.reader(myFile, delimiter=";", quotechar='"')
 
     for row in reader:
@@ -13,20 +33,57 @@ def readFunc():
     return(list)
 
 
-def writeFunc(list):
-
-    myFile = open("tarefas.txt", "w")
+def writeFunc(allLists):
+    dir = openTxt(r"\tarefas.txt")
+    myFile = open(os.path.join(dir), "w")
     writer = csv.writer(myFile, delimiter=';', quotechar='"', quoting=csv.QUOTE_ALL)
 
-    for row in list:
-        writer.writerow(row)
+    for row in allLists:
+        print("*",row.lista)
+        for elem in row.lista:
+            print("+",elem)
+            writer.writerow(elem)
     myFile.close()
+
+def readNomeListas():
+    list = []
+    dir = openTxt(r"\listaTarefas.txt")
+    myFile = open(os.path.join(dir), "r")
+    reader = csv.reader(myFile, delimiter=";", quotechar='"')
+
+    for row in reader:
+        if row:
+            list = list + [row]
+    myFile.close()
+    return (list)
+
+
+def writeNomeListas(alllists):
+    dir = openTxt(r"\listaTarefas.txt")
+    myFile = open(os.path.join(dir), "w")
+    writer = csv.writer(myFile, delimiter=';', quotechar='"', quoting=csv.QUOTE_ALL)
+
+    for row in alllists:
+        s = [row.nome, row.num]
+        print (s)
+        writer.writerow(s)
+    myFile.close()
+
+
+def getListas(tarefas, nomeListas):
+    allLists = []
+    x = 0
+    for row in nomeListas:
+        y = x + int(row[1])
+        allLists = allLists + [myLists(row[0], row[1], tarefas[x:y])]
+        x = x + int(row[1])
+    return allLists
 
 
 def consultFunc(list,funcao):
     # funcao pode ser main ou sort
 
-    for row in list:
+    for row in list.lista:
         if (row[3] == "Fechada" and funcao == "sort") or funcao == "main":
             print(row[0])
             print("Data de Criação:",row[1])
@@ -40,7 +97,7 @@ def consultFunc(list,funcao):
 
 def removeFunc(list):
     sel = 1
-    for row in list:
+    for row in list.lista:
         print(sel,")",row[0])
         sel = sel + 1
     print("Selecione a tarefa que pretende remover:")
@@ -48,7 +105,8 @@ def removeFunc(list):
     op = int(input())
     op =  op - 1
 
-    list.remove(list[op])
+    list.lista.remove(list.lista[op])
+    list.num = str(int(list.num) - 1)
 
     return(list)
 
@@ -70,16 +128,17 @@ def addTarefa(list):
         obs2 = ""
     temp = temp + [obs2]
 
-    list = list + [temp]
+    list.lista = list.lista + [temp]
+    list.num = str(int(list.num) + 1)
     return list
 
-def closeTarefa(list):
+def closeTarefa(list):  #mudar list para list.lista
     temp = []
     sel = 1
 
-    for i in range(0,len(list)):
-        if list[i][3] == "Aberta":
-            print(sel,")",list[i][0])
+    for i in range(0,len(list.lista)):
+        if list.lista[i][3] == "Aberta":
+            print(sel,")",list.lista[i][0])
             temp = temp + [i]
             sel = sel + 1
     if sel == 1:
@@ -87,20 +146,17 @@ def closeTarefa(list):
     else:
         print("Escolhe a tarefa que quer fechar:")
         sel = int(input()) - 1
-        list[temp[sel]][2] = datetime.datetime.now()
-        list[temp[sel]][3] = "Fechada"
-
+        list.lista[temp[sel]][2] = datetime.datetime.now()
+        list.lista[temp[sel]][3] = "Fechada"
     return(list)
 
-def estadoTarefa(list):
-    temp = []
-
+def estadoTarefa(list):  #mudar list para list.lista
     print("Deseja ver as tarefas:")
     print("1. Abertas")
     print("2. Fechadas")
     op = int(input())
 
-    for row in list:
+    for row in list.lista:
         if (op == 1) and (row[3]== "Aberta"):
             print(row[0])
             print("Data de Criação:", row[1])
@@ -114,32 +170,33 @@ def estadoTarefa(list):
             print("Observações:", row[4])
             print("------------------------------------------------------")
 
-def editTarefa(list):
+def editTarefa(list): #mudar list para list.lista
     sel = 1
 
     print("Selecione a lista que pretende editar:")
-    for row in list:
+    for row in list.lista:
         print(sel, ")", row[0] )
         sel = sel + 1
 
     sel = int(input()) -1
-    if list[sel][3] == "Fechada":
-        print("A tarefa esta fechada tem a certeza que deseja modificar-la?")
+    if list.lista[sel][3] == "Fechada":
+        print("A tarefa esta fechada tem a certeza que deseja modificar-la? \n 1) sim, \n 2) nao")
         resp = input()
-        if resp == "nao":
-            return
+        if resp == "2":
+            return list
     print("Pretende mudar o nome ou as obeservações?")
     print("1. Nome")
     print("2. Observações")
     op = int(input())
     if op == 1:
         print("Escreva o novo nome!")
-        list[sel][0] = input()
+        list.lista[sel][0] = input()
     if op == 2:
         print("Escreva a nova observação!")
         list[sel][4] = input()
+    return list
 
-def searchTarefa(list):
+def searchTarefa(list): #mudar list para list.lista
     print("Digite:")
 
     x = input()
@@ -148,26 +205,26 @@ def searchTarefa(list):
 
     temp = []
 
-    for i in range(0, len(list)):
-        d = list[i][0].find(x)
+    for i in range(0, len(list.lista)):
+        d = list.lista[i][0].find(x)
         if d != -1:
-            print(sel, ")", list[i][0])
+            print(sel, ")", list.lista[i][0])
             temp = temp + [i]
             sel = sel + 1
 
     if sel != 1:
         print("Que tarefa deseja selecionar?")
         op = int(input()) -1
-        print(list[temp[op]][0])
-        print("Data de Criação:", list[temp[op]][1])
-        if list[temp[op]][3] == "Fechada":
-            print("Estado:", list[temp[op]][3], "-> Data de concretização", list[temp[op]][2])
+        print(list.lista[temp[op]][0])
+        print("Data de Criação:", list.lista[temp[op]][1])
+        if list.lista[temp[op]][3] == "Fechada":
+            print("Estado:", list.lista[temp[op]][3], "-> Data de concretização", list.lista[temp[op]][2])
         else:
-            print("Estado:", list[temp[op]][3])
-        print("Observações:", list[temp[op]][4])
+            print("Estado:", list.lista[temp[op]][3])
+        print("Observações:", list.lista[temp[op]][4])
         print("------------------------------------------------------")
 
-def sortTarefa(list):
+def sortTarefa(list): #mudar list para list.lista
     campo = 0
     print("Quer organizar por: \n 1) nome, \n 2) data de criação \n 3) data de concretisação")
     op = int(input())
@@ -183,11 +240,11 @@ def sortTarefa(list):
         i = 0
         changed = False
 
-        while i < len(list)-1:
-            if (list[i+1][campo] < list[i][campo]):
-                aux = list[i]
-                list[i] = list[i+1]
-                list[i+1] = aux
+        while i < len(list.lista)-1:
+            if (list.lista[i+1][campo] < list.lista[i][campo]):
+                aux = list.lista[i]
+                list.lista[i] = list.lista[i+1]
+                list.lista[i+1] = aux
                 changed = True
 
             i = i+1
@@ -197,4 +254,30 @@ def sortTarefa(list):
 
     consultFunc(list,"sort")
 
-print (list)
+def atualiza(allLists, lista):
+    for i in range(0,len(allLists)):
+        if allLists[i].nome == lista.nome:
+            allLists[i] = lista
+    return allLists
+
+def mudarLista(allLists):
+    sel = 1
+    leng = len(allLists)
+    print("len",leng)
+    for list in allLists:
+        print(sel, ')', list.nome)
+        sel = sel + 1
+    op=int(input("Qual e que deseja mudar:"))
+    op = op - 1
+    list = allLists[op]
+    return list
+
+def mudarNomeLista(list): #list.nome
+    nome = input("Novo nome da lista:")
+    list.nome = nome
+    return list
+
+def criarList(allLists):
+    nome = input("Qual o nome da lista:")
+    allLists.append(myLists(nome,"0",[]))
+    return allLists
--- menuTask.py
+++ menuTask.py
@@ -1,4 +1,5 @@
-from auxTarefas import readFunc, writeFunc, consultFunc, removeFunc, addTarefa, closeTarefa, estadoTarefa, editTarefa, searchTarefa, sortTarefa
+from auxTarefas import readFunc, writeFunc, consultFunc, removeFunc, addTarefa, closeTarefa, estadoTarefa, editTarefa, searchTarefa, sortTarefa, readNomeListas, getListas, atualiza, mudarNomeLista, mudarLista, criarList, writeNomeListas
+import os
 
 def printMenu():
     print("1. Adicionar uma tarefa ")
@@ -9,23 +10,37 @@ def printMenu():
     print("6. Remover uma tarefa ")
     print("7. Procurar tarefas por nome ")
     print("8. Ordenação da lista de tarefas ")
-    print("9. Sair ")
+    print("9. Mudar lista atual")
+    print("10 Mudar nome da lista atual")
+    print("11. Criar nova lista")
+    print("12. Sair ")
 
 def main():
-    list = readFunc()
-
+    tarefas = readFunc()
+    nomesListas = readNomeListas()
+    allLists = getListas(tarefas, nomesListas)
+    i = 0
+    for x in allLists:
+        print(i," - ",x.nome, x.num, x.lista)
+        i = i + 1
+    list = allLists[0]
+    print(list.nome, list.num, list.lista)
+    print(len(allLists))
     printMenu()
-
     op= int(input("O que deseja selecionar:"))
 
-    while op != 9:
+    while op != 12:
         if op == 1:
             print("1. Adicionar uma tarefa ")
+            print(list.nome, list.num, list.lista)
             list = addTarefa(list)
+            print(list.nome, list.num, list.lista)
+            #atualiza(allLists, list)
 
         if op == 2:
             print("2. Editar uma tarefa ")
-            editTarefa(list)
+            list = editTarefa(list)
+            atualiza(allLists, list)
 
         if op == 3:
             print("3. Consultar tarefas ")
@@ -36,12 +51,14 @@ def main():
             estadoTarefa(list)
 
         if op == 5:
-            print("5. Marcar uma tarefa como fechada ")
-            closeTarefa(list)
+           print("5. Marcar uma tarefa como fechada ")
+           list = closeTarefa(list)
+           atualiza(allLists, list)
 
         if op == 6:
             print("6. Remover uma tarefa ")
             list = removeFunc(list)
+            atualiza(allLists, list)
 
         if op == 7:
             print("7. Procurar tarefas por nome ")
@@ -52,12 +69,28 @@ def main():
             sortTarefa(list)
 
         if op == 9:
-            print("9. Sair ")
+            print("9. Mudar lista atual")
+            list = mudarLista(allLists)
+            atualiza(allLists, list)
+
+        if op == 10:
+            print("10. Mudar nome lista atual")
+            list = mudarNomeLista(list)
+            atualiza(allLists, list)
+
+        if op == 11:
+            print("11. Criar nova lista")
+            allLists = criarList(allLists)
+
+
+        if op == 12:
+            print("12. Sair ")
 
         print("Carregue no 0 para retroceder")
         input()
         printMenu()
         op = int(input("O que deseja selecionar:"))
 
-    writeFunc(list)
+    writeFunc(allLists)
+    writeNomeListas(allLists)
 main()
